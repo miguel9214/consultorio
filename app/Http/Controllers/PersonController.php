fbba4fbea59848232b6c61c6de98f58577861051
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2024-01-06 16:19:23 -0500

    Creacion de persona ok

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -12,10 +12,9 @@ class PersonController extends Controller
      */
     public function index()
     {
-        $persons=Person::all();
+        $persons = Person::all();
 
-        return response()->json(['message'=>'List person','data'=>$persons]);
-        
+        return response()->json(['message' => 'List person', 'data' => $persons]);
     }
 
     /**
@@ -23,6 +22,7 @@ class PersonController extends Controller
      */
     public function create()
     {
+
         //
     }
 
@@ -31,7 +31,44 @@ class PersonController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'type_document' => 'required|string',
+            'document' => 'required|numeric',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'sex' => 'required|string',
+            'phone' => 'required|numeric',
+            'birthdate' => 'required|date',
+            'address' => 'required|string',
+            'city' => 'required|string',
+            'state' => 'required|string',
+            'neighborhood' => 'required|string',
+            'created_by_user' => 'required|numeric',
+            'updated_by_user' => 'required|numeric'
+
+        ]);
+
+        $person = Person::create([
+            'type_document' => $request->input('type_document'),
+            'document' => $request->input('document'),
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'sex' => $request->input('sex'),
+            'phone' => $request->input('phone'),
+            'birthdate' => $request->input('birthdate'),
+            'address' => $request->input('address'),
+            'city' => $request->input('city'),
+            'state' => $request->input('state'),
+            'neighborhood' => $request->input('neighborhood'),
+            'created_by_user' => $request->input('created_by_user'),
+            'updated_by_user' => $request->input('updated_by_user')
+        ]);
+
+        if ($person) {
+            return response()->json(['message' => 'Person created successfully']);
+        } else {
+            return response()->json(['message' => 'Error']);
+        }
     }
 
     /**
