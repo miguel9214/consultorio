1165dbb7db3732a3b632e29a75a45b8652789991
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-01-11 14:35:25 -0500

    modificacion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,9 +36,9 @@ class AuthController extends Controller
         // Inicio de la Transacción (Db::beginTransaction): Se inicia una transacción antes de realizar cualquier operación que forme parte de ella.
 
         // Operaciones en la Base de Datos: Se realizan varias operaciones de lectura o escritura en la base de datos como parte de la transacción.
-        
+
         // **Confirmación de la Transacción (Db::commit): Si todas las operaciones se han completado con éxito, se utiliza la función Db::commit` para confirmar la transacción. Esto significa que todas las operaciones realizadas en la transacción se guardan permanentemente en la base de datos.
-        
+
         // **Rollback de la Transacción (Db::rollBack): Si algo sale mal en cualquier punto de la transacción, puedes utilizar la función Db::rollBack` para deshacer todas las operaciones realizadas desde el inicio de la transacción, llevando la base de datos de nuevo a su estado original.
 
 
@@ -48,23 +48,22 @@ class AuthController extends Controller
             $user = new User();
             $user->rol = "paciente";
             $user->email = $request->email;
-            $user->password =  Hash::make($request->password);
+            $user->password = Hash::make($request->password);
             $user->save();
 
-            $person = Person::create([
-                'type_document' => $request->input('type_document'),
-                'document' => $request->input('document'),
-                'first_name' => $request->input('first_name'),
-                'last_name' => $request->input('last_name'),
-                'sex' => $request->input('sex'),
-                'phone' => $request->input('phone'),
-                'birthdate' => $request->input('birthdate'),
-                'address' => $request->input('address'),
-                'city' => $request->input('city'),
-                'state' => $request->input('state'),
-                'neighborhood' => $request->input('neighborhood'),
-                "user_id" => $user->id
-            ]);
+            $person = new Person();
+            $person->type_document = $request->type_document;
+            $person->document = $request->document;
+            $person->first_name = $request->first_name;
+            $person->last_name = $request->last_name;
+            $person->sex = $request->sex;
+            $person->phone = $request->phone;
+            $person->birthdate = $request->birthdate;
+            $person->address = $request->address;
+            $person->city = $request->city;
+            $person->state = $request->state;
+            $person->neighborhood = $request->neighborhood;
+            $person->user_id = $user->id;
             $person->save();
 
             $patients = new Pacient();
@@ -75,10 +74,9 @@ class AuthController extends Controller
 
             DB::commit();
             return response()->json(['message' => 'Usuario created successfully']);
-
         } catch (\Throwable $th) {
             DB::rollBack();
-            return response()->json(['message' => $th->getMessage()],422);
+            return response()->json(['message' => $th->getMessage()], 422);
         }
     }
 
