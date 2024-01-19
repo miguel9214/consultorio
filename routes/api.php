151d9e118fc105899ce0851144d47cae3a352201
Miguel Ramos <miguel921433@gmail.com>
2024-01-19 14:57:38 -0500

    conflictos en rutas

--- routes/api.php
+++ routes/api.php
@@ -9,20 +9,17 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => 'auth:api'], function () {
 
-
     Route::post('logout', [AuthController::class, 'logout']);
-<<<<<<< HEAD
-    Route::get('user-profile', [AuthController::class, 'userProfile']);
-    
-=======
 
->>>>>>> 534a01553388d171014dffc1593f76910c908156
+    Route::get('user-profile', [AuthController::class, 'userProfile']);
+
+
     //EPS
     Route::get('eps', [EpsController::class, 'index']);
-    // Route::get('eps/{id}', [EpsController::class, 'show']);
-    //Route::post('eps', [EpsController::class, 'store']);
-    // Route::put('eps/{id}', [EpsController::class, 'update']);
-    // Route::delete('eps/{id}', [EpsController::class, 'destroy']);
+    Route::get('eps/{id}', [EpsController::class, 'show']);
+    Route::post('eps', [EpsController::class, 'store']);
+    Route::put('eps/{id}', [EpsController::class, 'update']);
+    Route::delete('eps/{id}', [EpsController::class, 'destroy']);
 });
 
 Route::apiResource("v1/persons", PersonController::class);
@@ -34,11 +31,5 @@ Route::post('login', [AuthController::class, 'login']);
 Route::get('users', [AuthController::class, 'allUsers']);
 
 Route::get('epsPublic', [EpsController::class, 'indexPublic']);
-<<<<<<< HEAD
 
-=======
-Route::get('eps/{id}', [EpsController::class, 'show']);
-Route::post('eps', [EpsController::class, 'store']);
-Route::put('eps/{id}', [EpsController::class, 'update']);
-Route::delete('eps/{id}', [EpsController::class, 'destroy']);
->>>>>>> 534a01553388d171014dffc1593f76910c908156
+
