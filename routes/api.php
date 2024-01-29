d8bfad04aad5c676fdeb009730a4fd226f46d3f8
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-01-29 15:36:24 -0500

    modificacion routes

--- routes/api.php
+++ routes/api.php
@@ -12,14 +12,14 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => 'auth:api'], function () {
 
-    
+
     //CONSULTATION
     Route::get('consultation', [ConsultationController::class, 'index']);
     Route::get('consultation/{id}', [ConsultationController::class, 'show']);
     Route::post('consultation', [ConsultationController::class, 'store']);
     Route::put('consultation/{id}', [ConsultationController::class, 'update']);
     Route::delete('consultation/{id}', [ConsultationController::class, 'destroy']);
-    
+
     //CONSULTATION_TYPE
     Route::get('consultationType', [ConsultationtypeController::class, 'index']);
     Route::get('consultationType/{id}', [ConsultationtypeController::class, 'show']);
@@ -33,6 +33,13 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('eps', [EpsController::class, 'store']);
     Route::put('eps/{id}', [EpsController::class, 'update']);
     Route::delete('eps/{id}', [EpsController::class, 'destroy']);
+
+    //MEDICO
+    Route::get('medico', [MedicoController::class, 'index']);
+    Route::get('medico/{id}', [MedicoController::class, 'show']);
+    Route::post('medico', [MedicoController::class, 'store']);
+    Route::put('medico/{id}', [MedicoController::class, 'update']);
+    Route::delete('medico/{id}', [MedicoController::class, 'destroy']);
 
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('userProfile', [AuthController::class, 'userProfile']);
@@ -49,10 +56,3 @@ Route::post('login', [AuthController::class, 'login']);
 Route::get('users', [AuthController::class, 'allUsers']);
 
 Route::get('epsPublic', [EpsController::class, 'indexPublic']);
-
-//MEDICO
-Route::get('medico', [MedicoController::class, 'index']);
-Route::get('medico/{id}', [MedicoController::class, 'show']);
-Route::post('medico', [MedicoController::class, 'store']);
-Route::put('medico/{id}', [MedicoController::class, 'update']);
-Route::delete('medico/{id}', [MedicoController::class, 'destroy']);
