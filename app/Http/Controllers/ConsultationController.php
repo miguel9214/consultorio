e57b179d3e5dc94bf844fe8028d1df4c48ee3a6a
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-02-08 15:43:54 -0500

    Modificaciones

--- app/Http/Controllers/ConsultationController.php
+++ app/Http/Controllers/ConsultationController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ConsultationType;  
-use App\Models\Medico;            
+use App\Models\ConsultationType;
+use App\Models\Medico;
 use App\Models\Pacient;
 use App\Models\Consultation;
 use Illuminate\Http\Request;
@@ -25,67 +25,37 @@ class ConsultationController extends Controller
             ->join("persons as p", "p.id", "d.person_id")
             ->join("patients as pc", "pc.id", "c.pacient_id")
             ->join("persons as ps", "ps.id", "pc.person_id")
-            ->join("users as us", "us.id", "ps.user_id")
             ->select(
                 DB::raw("CONCAT(p.first_name, ' ', p.last_name) as doctor"),
                 DB::raw("CONCAT(ps.first_name, ' ', ps.last_name) as paciente"),
+                "c.id",
                 "tc.name as tipo_consulta",
                 "c.date as fecha",
                 "c.hour as hora",
                 "c.observation as observacion",
                 "c.status as estado",
-                "c.id as id"
             )
             ->get();
-    
-        return response()->json(['message' => 'List of Consultation INDEX', 'data' => $data]);
+
+        return response()->json(['message' => 'List of Consultation', 'data' => $data]);
     }
-    
-    // public function show(string $id)
-    // {
-    //     $data = DB::table("consultations as c")
-    //         ->join("consultation_types as tc", "tc.id", "c.consultation_type_id")
-    //         ->join("doctors as d", "d.id", "c.doctor_id")
-    //         ->join("persons as p", "p.id", "d.person_id")
-    //         ->join("patients as pc", "pc.id", "c.pacient_id")
-    //         ->join("persons as ps", "ps.id", "pc.person_id")
-    //         ->join("users as us", "us.id", "ps.user_id")
-    //         ->join("invoices as in", "c.id", "in.consultation_id")
-    //         ->select(
-    //             DB::raw("CONCAT(p.first_name, ' ', p.last_name) as doctor"),
-    //             DB::raw("CONCAT(ps.first_name, ' ', ps.last_name) as paciente"),
-    //             "c.id",                
-    //             "c.date as fecha",
-    //             "c.hour as hora",
-    //             "c.observation as observacion",
-    //             "c.status as estado",
-    //             "tc.name as tipo_consulta",
-    //             "tc.price as price",
-    //             "ps.address as direccion",
-    //             "ps.phone as telefono",
-    //             "us.email as correo",
-    //             "in.invoice_number as invoice_number",
-    //             "in.start_date as start_date",
-    //             "in.due_date as due_date",
-    //             "in.status as status",
-    //             "in.total_amount as total_amount",
-    //             "in.taxes as taxes",
-    //             "in.discounts as discounts",
-    //             "in.amount_paid as amount_paid",
-    //         )
-    //         ->get();
-    
-    //     return response()->json(['message' => 'Consultation Found in ShowlIST', 'data' => $data]);
-    // }
+
+
+    public function indexPublic()
+    {
+        $consultationList = Consultation::all();
+
+        return response()->json(['message' => 'List of Consultation', 'data' => $consultationList]);
+    }
 
     public function show(string $id)
     {
-        $Consultation = Consultation::find($id);
+        $consultation = Consultation::find($id);
 
-        if ($Consultation) {
-            return response()->json(['message' => 'Consultation show found', 'data' => $Consultation]);
+        if ($consultation) {
+            return response()->json(['message' => 'Consultation found', 'data' => $consultation]);
         } else {
-            return response()->json(['message' => 'Consultation show not found']);
+            return response()->json(['message' => 'Consultation not found']);
         }
     }
 
@@ -100,17 +70,17 @@ class ConsultationController extends Controller
             'doctor_id' => 'required|int',
             'pacient_id' => 'required|int'
         ]);
-        
+
         try {
-        
+
             $consultation = new Consultation();
             $consultation->observation = $request->observation;
             $consultation->status = $request->status;
-            $consultation->date = $request->date; 
-            $consultation->hour = $request->hour;  
+            $consultation->date = $request->date;
+            $consultation->hour = $request->hour;
             $consultation->consultation_type_id = $request->consultation_type_id;
             $consultation->doctor_id = $request->doctor_id;
-            $consultation->pacient_id = $request->pacient_id;          
+            $consultation->pacient_id = $request->pacient_id;
             $consultation->created_by_user = Auth::id();
             $consultation->updated_by_user = Auth::id();
             $consultation->save();
@@ -137,11 +107,11 @@ class ConsultationController extends Controller
             $consultation = Consultation::find($id);
             $consultation->observation = $request->observation;
             $consultation->status = $request->status;
-            $consultation->date = $request->date; 
-            $consultation->hour = $request->hour;   
+            $consultation->date = $request->date;
+            $consultation->hour = $request->hour;
             $consultation->consultation_type_id = $request->consultation_type_id;
             $consultation->doctor_id = $request->doctor_id;
-            $consultation->pacient_id = $request->pacient_id;          
+            $consultation->pacient_id = $request->pacient_id;
             $consultation->updated_by_user = Auth::id();
             $consultation->save();
 
