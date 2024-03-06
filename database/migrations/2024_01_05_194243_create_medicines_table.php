0017153cd824b93fb2c2c5bdf09563076e79c875
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-03-06 16:51:05 -0500

    Modificaciones

--- database/migrations/2024_01_05_194243_create_medicines_table.php
+++ database/migrations/2024_01_05_194243_create_medicines_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
             $table->unsignedInteger("id")->autoIncrement();
             $table->string("code");
             $table->string("name");
+            $table->string("dosage");
+            $table->string("dosing_frequency");
+            $table->string("indications");
+            $table->string("contraindications");
+            $table->string("administration_method");
+            $table->string("pharmaceutical_laboratory");
+            $table->float('price');
             $table->timestamps();
         });
     }
